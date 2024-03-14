47bae1ff3c463b75479aa59a6fc571433a99cf91
zvzannaj <zuzannajaroszczyk2@gmail.com>
2024-03-14 20:57:50 +0100

    zadanie 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, main"); 
+﻿static double GetAverage(int[] numbers)
+{
+    double result = 0.0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+
+    return result / numbers.Length;
+}
