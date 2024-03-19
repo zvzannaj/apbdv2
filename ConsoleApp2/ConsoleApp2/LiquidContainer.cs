c3a7b7cacbc6cc424458d01a07d3aa66c4584730
zvzannaj <zuzannajaroszczyk2@gmail.com>
2024-03-19 21:10:13 +0100

    liquidcointainer

---
+++ ConsoleApp2/ConsoleApp2/LiquidContainer.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2;
+
+public class LiquidContainer : Container
+{
+    public LiquidContainer(double cargoWeight) : base(cargoWeight)
+    {
+        
+    }
+    
+    public override void Load(double cargoWeight)
+    {
+        base.Load(cargoWeight);
+        throw new NotImplementedException();
+    }
+}
